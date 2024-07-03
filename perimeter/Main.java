c0bcfe5fb379fd527d0b6990d56576128b5b70cf
madalinadumitru <madalina.vlad94@gmail.com>
2024-07-03 23:13:34 +0300

    code + exercise

---
+++ perimeter/Main.java
@@ -0,0 +1,68 @@
+package debuggingexercises.Debugging.perimeterchallange;
+
+import debuggingexercises.Debugging.perimeterchallange.Point;
+
+public class Shape {
+
+    private Point[] points;
+    private int numberOfPoints;
+
+    public Shape() {
+        this.points = new Point[10];
+    }
+
+    public Point[] getPoints() {
+        return points;
+    }
+
+    public void setPoints(Point[] points) {
+        this.points = points;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
+    }
+
+    public void setNumberOfPoints(int numberOfPoints) {
+        this.numberOfPoints = numberOfPoints;
+    }
+
+    public int getBiggestX () throws Exception {
+        if (numberOfPoints == 0) {
+            throw new Exception("Nu sunt puncte in forma");
+        }
+        int maxX = points[0].getX();
+        for (int i = 1; i < numberOfPoints; i++) {
+            if (points[i].getX() > maxX) {
+                maxX = points[i].getX();
+            }
+        }
+        return maxX;
+    }
+
+    public double getLongestSideLength () throws Exception {
+        if (numberOfPoints <= 1) {
+            throw new Exception("Nu se poate calcula o latura");
+        }
+        double maxLength = points[0].distanceTo(points[1]);
+        for (int i = 1; i < numberOfPoints-1; i++) {
+            if (points[i].distanceTo(points[i+1]) > maxLength) {
+                maxLength = points[i].distanceTo(points[i+1]);
+            }
+        }
+        return maxLength;
+    }
+
+    public double getAverageDistanceBetweenPoints () throws Exception {
+        if (numberOfPoints <= 1) {
+            throw new Exception("Nu se poate calcula o latura");
+        }
+        double totalDistance = 0;
+        int count = 0;
+        for (int i = 0; i < numberOfPoints - 1; i++) {
+            totalDistance += points[i].distanceTo(points[i+1]);
+            count++;
+        }
+        return totalDistance / count;
+    }
+}
---
+++ perimeter/Point.java
@@ -0,0 +1,47 @@
+package debuggingexercises.Debugging.perimeterchallange;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distanceTo (Point otherPt) {
+        int dx = x - otherPt.getX();
+        int dy = y - otherPt.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+//    public double distanceTo2 (Point p1, Point p2) {
+//        int dx = p1.getX() - p2.getX();
+//        int dy = p1.getY() - p2.getY();
+//        return Math.sqrt(dx * dx + dy * dy);
+//    }
+    //se apeleaza p1.distanceTo2(p1,p2);
+
+    public static double distanceTo2 (Point p1, Point p2) {
+        int dx = p1.getX() - p2.getX();
+        int dy = p1.getY() - p2.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    //Se apeleaza pe clasa Points.distranceTo2(p1,p2);
+}
